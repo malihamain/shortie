b60525de2a7f8d7a86c157118410d09f60f2c377
malihamain <amaliharahmani@gmail.com>
2025-11-20 16:07:03 +0700

    Fix: Add module path resolution for Netlify function dependencies

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,3 +1,14 @@
+// Ensure we can find dependencies from root
+const path = require('path')
+const Module = require('module')
+const originalRequire = Module.prototype.require
+
+// Add root node_modules to module path
+const rootPath = path.resolve(__dirname, '../..')
+if (!Module._nodeModulePaths.includes(path.join(rootPath, 'node_modules'))) {
+    Module._nodeModulePaths.unshift(path.join(rootPath, 'node_modules'))
+}
+
 const serverless = require('serverless-http')
 const app = require('../../server')
 
